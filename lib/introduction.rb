98631db37e18c7d8da5a38a8b5ccc2562ba91e18
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-09-09 02:35:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,4 +3,4 @@ puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-puts phrase: `"Hi, my name is #{name} and I am learning to program in #{language}."
+puts  `"Hi, my name is #{name} and I am learning to program in #{language}."
